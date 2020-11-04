bcaf513151c41a223c47b0de3643185334a59404
Alejandro Martínez <alejandro2ml22@gmail.com>
2020-11-03 20:30:16 -0600

    Seed con analista y empresa de prueba

--- database/seeds/TipoCuentaSeeder.php
+++ database/seeds/TipoCuentaSeeder.php
@@ -21,6 +21,35 @@ class TipoCuentaSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now()
         ]);
-        
+        //Sectores Seeder
+        DB::table('sector')->insert([
+            'nombre'=>'Industrial',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('sector')->insert([
+            'nombre'=>'Minero',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('sector')->insert([
+            'nombre'=>'Ganaderia y Acuicultura',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('sector')->insert([
+            'nombre'=>'Servicios',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('empresa')->insert([
+            'nombre'=>'Empresa de prueba',
+            'nit'=>'Nit de prueba',
+            'nrc'=>'Nrcde de prueba',
+            'sector_id'=>1,
+            'user_id'=>2,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
